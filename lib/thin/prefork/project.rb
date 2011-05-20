b90cd052042f8256e6fa321a0cde622606c56690
Daniel Barlow <dan@telent.net>
2011-05-20 09:45:51 +0100

    rewrite the projectr class to be saner and less surprising

--- lib/thin/prefork/project.rb
+++ lib/thin/prefork/project.rb
@@ -1,42 +1,21 @@
 class Thin::Prefork::Project < Thin::Prefork
-  module WorkerHelper
-    def start!
-      Projectr::Project[self.project].load!
-      super
-    end
-    def reload!
-      Projectr::Project[self.project].load!
-      super
+
+  # We define #project= and #project methods. Because the parent uses
+  # T::P::NamedArgs, this allows :project to be used as an initarg
+  def project=(project)
+    @project=Projectr::Project[project]
+    inotifier=@project.watch_files do |p|
+      self.reload
     end
   end
-  def initialize(args)
-    project=args.delete(:project)
-    super
-    # the purpose of this metagymnastics is to create a new subclass
-    # of Thin::Prefork::Worker which has access to the values for
-    # +app_class+ and +project+ that were passed as arguments to this
-    # constructor.  If you know of a better way, advice/patches welcomed
-    parent=self.worker_class || Thin::Prefork::Worker
-    w=Class.new(parent)
-    w.class_eval do
-      include WorkerHelper
-    end
-    w.send(:define_method,:project,Proc.new {|| project })
-    self.worker_class=w
-
-    # ask the project for the inotifier that will call us when its
-    # files change
-    p= ::Projectr::Project[project]
-    master=self
-    inotifier=p.watch_files do |project,name|
-      master.reload!
-    end
-
-    # register the filehandle for this notifier with the event loop
-    # in #run!  The block is called when inotifier.to_io is "ready"
-    # for some class of I/O operation
-    master.add_io_handler(inotifier.to_io) do |direction|
-      inotifier.process
-    end
+  def project
+    @project
   end
+
+  def reload
+    self.workers.map(&:stop)
+    self.project.load!
+    self.workers.map(&:start)
+  end
+
 end
